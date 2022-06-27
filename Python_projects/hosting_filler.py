bf91583d1615037709be4af2896991c5d0282fd1
MikkelKaiser <mikkelkaiserandersen@hotmail.com>
2022-06-27 17:01:17 +0200

    Updated waits to implicitly_wait

--- Python_projects/hosting_filler.py
+++ Python_projects/hosting_filler.py
@@ -2,8 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import config
 
@@ -15,10 +13,9 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Open up site
 driver.get("https://suite.adnami.io")
-# wait 1 second
-time.sleep(2)
 
 # get email, password and btn input fields
+driver.implicitly_wait(5)
 email = driver.find_element(By.ID, "Input_Email")
 password = driver.find_element(By.ID, "Input_Password")
 submit_btn = driver.find_element(By.CLASS_NAME, "btn")
@@ -29,34 +26,34 @@ email.send_keys(config.emailadress)
 password.send_keys(config.password)
 # Submit the login
 submit_btn.click()
-time.sleep(2)
+
 
 # After login go to hosting url
-driver.get("https://suite.adnami.io/hosting")
 time.sleep(2)
+driver.get("https://suite.adnami.io/hosting")
 
 # Get desired selected element and dropdown
-
+driver.implicitly_wait(5)
 organisation_dropdown = driver.find_elements(By.XPATH, "(//div[@class='multiselect__tags'])[1]")
-time.sleep(0.5)
 selectElem = driver.find_element(By.XPATH, "//span[contains(text(),'Adnami Development')]")
+
 # open dropdown
 organisation_dropdown[0].click()
-time.sleep(1)
 # Select 'Adnami Development'
 selectElem.click()
 
 # Open second dropdown
+driver.implicitly_wait(2)
 second_drop = driver.find_elements(By.XPATH, "(//div[@class='multiselect__tags'])[2]")
 second_drop[0].click()
-time.sleep(2)
+
+driver.implicitly_wait(5)
 # Select 'Certification Ads'
 select_drop2 = driver.find_element(By.XPATH, "//span[contains(text(),'Certification Ads')]")
 select_drop2.click()
 
 # Fill in the campaign name
 campaign_name = driver.find_element(By.XPATH, "(//input[@type='text'])[4]")
-time.sleep(0.5)
 campaign_name.send_keys(" - Hosting")
 
 # campaign_description = driver.find_element(By.ID, 'input-81')
