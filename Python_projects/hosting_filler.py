50de4afdb0fe567d18bb83c8764d53169b9eb526
MikkelKaiser <mikkelkaiserandersen@hotmail.com>
2022-06-27 16:50:22 +0200

    Updated hosting_filler, new_publisher

--- Python_projects/hosting_filler.py
+++ Python_projects/hosting_filler.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import config
 
@@ -10,8 +11,6 @@ import config
 # Download your chrome driver for script here => https://chromedriver.chromium.org/downloads
 # PATH = "C:/Users/Mikke/OneDrive/Desktop/Python_project/asd/chrome_driver/chromedriver.exe"
 
-options = Options()
-options.add_argument("start-maximized")
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 # Open up site
@@ -37,27 +36,40 @@ driver.get("https://suite.adnami.io/hosting")
 time.sleep(2)
 
 # Get desired selected element and dropdown
-organisation_dropdown = driver.find_elements(By.CLASS_NAME, "multiselect.organization-filter-input")
-selectElem = driver.find_element(By.XPATH, "//span[text()='Adnami Development']")
-time.sleep(1)
+
+organisation_dropdown = driver.find_elements(By.XPATH, "(//div[@class='multiselect__tags'])[1]")
+time.sleep(0.5)
+selectElem = driver.find_element(By.XPATH, "//span[contains(text(),'Adnami Development')]")
 # open dropdown
 organisation_dropdown[0].click()
-time.sleep(0.5)
+time.sleep(1)
 # Select 'Adnami Development'
 selectElem.click()
-time.sleep(1)
 
 # Open second dropdown
-organisation_dropdown[1].click()
-time.sleep(1)
-# Select 'Certification Ads'
-selectFilter = driver.find_element(By.XPATH, "//span[text()='Certification Ads']")
-selectFilter.click()
+second_drop = driver.find_elements(By.XPATH, "(//div[@class='multiselect__tags'])[2]")
+second_drop[0].click()
 time.sleep(2)
+# Select 'Certification Ads'
+select_drop2 = driver.find_element(By.XPATH, "//span[contains(text(),'Certification Ads')]")
+select_drop2.click()
 
 # Fill in the campaign name
-campaign_name = driver.find_element(By.ID, 'input-77')
+campaign_name = driver.find_element(By.XPATH, "(//input[@type='text'])[4]")
+time.sleep(0.5)
 campaign_name.send_keys(" - Hosting")
+
+# campaign_description = driver.find_element(By.ID, 'input-81')
+# time.sleep(0.5)
+# campaign_description.send_keys("Random desc")
+# time.sleep(1)
+
+# country_dropdown = driver.find_element(By.CLASS_NAME, "multiselect.country-filter-input")
+# selectCountry =  driver.find_element(By.XPATH, "//span[text()='Denmark']")
+# time.sleep(1)
+# country_dropdown.click()
+# time.sleep(1)
+# selectCountry.click()
 
 # Wait time before closing window
 time.sleep(40)
--- Python_projects/new_publisher.py
+++ Python_projects/new_publisher.py
@@ -1,15 +1,14 @@
 import os
 from os.path import exists
+from datetime import datetime
+import re
 import sys
 import fileinput
 
 publishers = os.listdir('C:/Users/Mikke/OneDrive/Desktop/Projekter/new-adnami-publishers/adnami-publishers/src/publishers')
-
-publisher_search = "publisher: ['manmn']"
-publisher_to_replace = "publisher: ['Unknown']"
-
-publisher_defined = "publisher:"
-
+cert_log_defined = "Certified format;Date modified;Updated by"
+publisher_to_replace = "publisher: ['MSO Digital']"
+publisher_defined = "publisher: ['MSO']"
 publisher_to_replace_spot = "}"
 default_publisher = "  publisher: ['Unknown'] // Added default publisher \n }"
 
@@ -17,6 +16,7 @@ default_publisher = "  publisher: ['Unknown'] // Added default publisher \n }"
 
 for publisher in publishers:
     fileToSearch  = 'C:/Users/Mikke/OneDrive/Desktop/Projekter/new-adnami-publishers/adnami-publishers/src/publishers/' + publisher + '/config.js'
+    update_cert_log = "C:/Users/Mikke/OneDrive/Desktop/Projekter/new-adnami-publishers/adnami-publishers/src/publishers/" + publisher + "/cert_log.csv"
     try:
         if exists(fileToSearch):
             with open(fileToSearch) as x:
@@ -27,24 +27,64 @@ for publisher in publishers:
                     filedata = f.read()
                     f.close()
 
-                    newdata = filedata.replace(publisher_search,publisher_to_replace)
+                    newdata = filedata.replace(publisher_defined,publisher_to_replace)
 
                     f = open(fileToSearch,'w')
                     f.write(newdata)
                     f.close()
+                    update_log(update_cert_log)
                  else:
+                    pass
                     # All the files that did NOT find any publishers in the config file
-                    f = open(fileToSearch,'r')
-                    filedata = f.read()
-                    f.close()
+                    # f = open(fileToSearch,'r')
+                    # filedata = f.read()
+                    # f.close()
 
-                    defaultPublisher = filedata.replace(publisher_to_replace_spot, default_publisher)
-                    f = open(fileToSearch,'w')
-                    f.write(defaultPublisher)
-                    f.close()
+                    # defaultPublisher = filedata.replace(publisher_to_replace_spot, default_publisher)
+                    # f = open(fileToSearch,'w')
+                    # f.write(defaultPublisher)
+                    # f.close()
         else:
             # Could not find config.js in the given PATH
             print("Config does not exist")
+
+        def update_log(update_cert_log):
+            date_today = datetime.today().strftime('%Y-%m-%d')
+            new_log_line = "Certified format;Date modified;Updated by\npublisher_information;" + date_today + ";mka\n"
+            if exists(update_cert_log):
+                # Get top line of cert document for replacement
+                with open(update_cert_log,"r") as log:
+                    for line in log:
+                        pattern = ";(.*?);"
+                        substring = re.search(pattern, line).group(1)
+                        if substring == '05-04-2022' or substring == '06-04-2022':
+                            f = open(update_cert_log, 'r')
+                            file_data = f.read()
+                            f.close()
+
+                            updated_cert_log = file_data.replace(substring, date_today)
+
+                            f = open(update_cert_log, 'w')
+                            f.write(updated_cert_log)
+                            f.close()
+
+                        if cert_log_defined in line:
+                            old_log_line = line
+
+                f = open(update_cert_log, 'r')
+                file_data = f.read()
+                f.close()
+
+                updated_cert_log = file_data.replace(old_log_line, new_log_line)
+
+                f = open(update_cert_log, 'w')
+                f.write(updated_cert_log)
+                f.close()
+            else:
+                # This gets hit if it couldn't find the cert_log file => create one and insert the data
+                f = open(update_cert_log, 'w')
+                f.write(new_log_line)
+                f.close()
     except:
         print("Error")
 
